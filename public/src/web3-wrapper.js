e3b5f1fbe6a4f97e509e2a2d07711458b07aeffe
OGZ <adi.gupta13@gmail.com>
2021-09-15 23:54:59 +0530

    Adding data fetch from web3

--- public/src/web3-wrapper.js
+++ public/src/web3-wrapper.js
@@ -1,17 +1,54 @@
 'use strict';
 
+import erc721 from '../compiled_contract/ERC721.js';
+import metadata from '../compiled_contract/Metadata.js';
+// import ethers from "./ethers.js";
+// import {Web3} from './ethers.js';
+
+
 export default class Web3Wrapper {
+
+  tunesAddress = '0xfa932d5cBbDC8f6Ed6D96Cc6513153aFa9b7487C'
+  metadataAddress = '0xD9692a84cC279a159305a4ef1A01eFab77B4Deb2'
+
+  provider
+  tunesContract
+  metadataContract
+  constructor() {
+    this.provider = new ethers.providers.InfuraProvider('homestead', 'eefe88ec80f74d33a52967249a8d4db1')
+    this.tunesContract = new ethers.Contract(this.tunesAddress, erc721, this.provider)
+    this.metadataContract = new ethers.Contract(this.metadataAddress, metadata, this.provider)
+  }
+
   async getOwnersTuneId() {
     return null;
   }
 
   async getTune(tuneId) {
+
+    console.log(ethers);  
+
+    let tuneOwner = await this.tunesContract.ownerOf(tuneId);
+
+    let tuneOfficialMetadata = await this.tunesContract.tokenURI(tuneId)
+
+    fetch(tuneOfficialMetadata).then(response => response.json()).then(json => {
+      console.log(json)
+    })
+
+    let response = await fetch(tuneOfficialMetadata);
+    let json = await response.json();
+
     return {
-      "name": "Your Tears Bring Snow in Cold Flurries",
-      "artist": "Donald Bough",
+      "name": json.name,
+      "owner": tuneOwner,
+      "ownerUrl": "https://opensea.io/" + tuneOwner,
+      "artist": "TODO",
       "album": "Tunes",
       "url": "./your-tears-breed-cold-flurries-song",
-      "cover_art_url": "temporary_album.png"
+      "cover_art_url": json.image
     }
   }
+
+
 }
